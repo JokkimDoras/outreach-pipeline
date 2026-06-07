331d00e0fb95d100ba093046c38a2b66d8774d32
Jokkim <jokkimxavier@gmail.com>
2026-06-07 20:29:17 +0530

    added the abort logic if no email was founded

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import chalk from "chalk";
 
 dotenv.config();
 
-console.log(chalk.yellow("━━━━━━━━━━━━━━━━━━━━━━"));
+console.log(chalk.cyan("━━━━━━━━━━━━━━━━━━━━━━"));
 console.log(chalk.green("Starting pipeline..."));
 
 async function getDomine(): Promise<string> {
@@ -37,23 +37,52 @@ async function confirm(question: string): Promise<boolean> {
 }
 
 async function main() {
+
   const domain = await getDomine();
   console.log(`Starting pipeline for: ${domain}`);
+                                                                                                                                                                                                                                                                                                                              
+  //get the user typed named
 
-  const companies = await findLookalikes(domain);
+
+  const companies = await findLookalikes(domain);  
   console.log(`Found ${companies.length} companies`);
+   //find all lookalike company for this user typed domain in ocean , then return
+  // name:string;
+  // domain:string;
+
 
   const decisionMakers = await findDecisionMakers(companies.slice(0, 2));
   console.log(`Found ${decisionMakers.length} decision makers`);
-
+//find all the decision makers in those lookalike company then get those array of  
+  //  name: string;
+  // title: string;
+  // email: string;-> empty string      then the  enrichemail function get the email and modified the eamil like '' -> mail.com
+  // company: string;
+  // domain: string;
+  // person_id:string;
+  
 
   const contacts = await enrichEmails(decisionMakers.slice(0, 1));
   console.log(`Got emails for ${contacts.length} contacts`);
+   //name: string;
+  // title: string;
+  // email: string;-> now its not empty string it has the decisionmaker mail ID
+  // company: string;
+  // domain: string;
+  // person_id:string;
+
 
   
-  // Safety checkpoint
-  console.log(chalk.yellow("\n⚠️  About to send emails to:"));
-  contacts.forEach(c => console.log(`  → ${c.name} <${c.email}> (${c.company})`));
+ 
+  // contacts.forEach(c => console.log(`  → ${c.name} <${c.email}> (${c.company})`));
+
+  if(contacts.length === 0){
+    console.log(chalk.red("No Email Found")) 
+    return;
+  } 
+  
+   // Safety checkpoint
+   console.log(chalk.yellow("\n⚠️  About to send emails to:"));
 
   const ok = await confirm(chalk.bold(`\nSend ${contacts.length} emails? (y/n): `));
 
